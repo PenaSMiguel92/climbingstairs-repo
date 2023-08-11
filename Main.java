0747c34204cc0e59512816649012abe8f5733158
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-11 15:49:40 -0700

    completed solution

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        for (int i = 1; i <= 45; i++) {
+            System.out.println(sol.climbStairs(i));
+        }
+    }
+}
