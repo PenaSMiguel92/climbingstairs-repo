2545c4df8d893656c446bcfcf109c31396799786
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-11 16:31:23 -0700

    stopped storing 1 and 2 to the hashmap and somehow got a higher memory usage. The system is rigged.

--- Solution.java
+++ Solution.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Solution {
     Map<Integer, Integer> prevValues = new HashMap<>();
     public int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+
         if (prevValues.containsKey(n))
             return prevValues.get(n);
 
-        if (n == 1 || n == 2) {
-            prevValues.put(n, n);
-            return n;
-        } 
         prevValues.put(n, climbStairs(n - 1) + climbStairs(n - 2));
         return prevValues.get(n);
     }
